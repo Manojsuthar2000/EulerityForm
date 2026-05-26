1c5adeba93d8410ec3e4f394791ebe8867837ad8
ManojSutharSuperMind <228002291+manojs-afk@users.noreply.github.com>
2026-05-26 16:45:46 +0530

    Initial Commit

---
+++ EulerityForm/EulerityFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityFormApp.swift
+//  EulerityForm
+//
+//  Created by Manoj Suthar on 26/05/26.
+//
+
+import SwiftUI
+
+@main
+struct EulerityFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
